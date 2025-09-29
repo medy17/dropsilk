2039263222f1b9ed2622a4a78c159e55980e1b9e
medy17 <88668010+medy17@users.noreply.github.com>
2025-09-29 14:24:30 +0800

    fixed code preview highlighting

--- frontend/src/js/preview/handlers/codePreview.js
+++ frontend/src/js/preview/handlers/codePreview.js
@@ -6,7 +6,18 @@ import python from 'highlight.js/lib/languages/python';
 import xml from 'highlight.js/lib/languages/xml';
 import css from 'highlight.js/lib/languages/css';
 import markdown from 'highlight.js/lib/languages/markdown';
-import 'highlight.js/styles/github.css';
+import java from 'highlight.js/lib/languages/java';
+import cpp from 'highlight.js/lib/languages/cpp';
+import c from 'highlight.js/lib/languages/c';
+import csharp from 'highlight.js/lib/languages/csharp';
+import go from 'highlight.js/lib/languages/go';
+import ruby from 'highlight.js/lib/languages/ruby';
+import php from 'highlight.js/lib/languages/php';
+import bash from 'highlight.js/lib/languages/bash';
+import yaml from 'highlight.js/lib/languages/yaml';
+import json from 'highlight.js/lib/languages/json';
+import typescript from 'highlight.js/lib/languages/typescript';
+import 'highlight.js/styles/github-dark.css';
 
 // Register languages
 hljs.registerLanguage('javascript', javascript);
@@ -14,6 +25,17 @@ hljs.registerLanguage('python', python);
 hljs.registerLanguage('xml', xml); // For HTML
 hljs.registerLanguage('css', css);
 hljs.registerLanguage('markdown', markdown);
+hljs.registerLanguage('java', java);
+hljs.registerLanguage('cpp', cpp);
+hljs.registerLanguage('c', c);
+hljs.registerLanguage('csharp', csharp);
+hljs.registerLanguage('go', go);
+hljs.registerLanguage('ruby', ruby);
+hljs.registerLanguage('php', php);
+hljs.registerLanguage('bash', bash);
+hljs.registerLanguage('yaml', yaml);
+hljs.registerLanguage('json', json);
+hljs.registerLanguage('typescript', typescript);
 
 
 export default async function renderCodePreview(blob, contentElement) {
