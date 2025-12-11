9c6d0f2b22eddbac1e51d7e94d8f41e027530247
medy17 <88668010+medy17@users.noreply.github.com>
2025-12-11 09:35:40 +0800

    fixed 404.html page

---
+++ frontend/vite.config.mjs
@@ -0,0 +1,19 @@
+import { defineConfig } from 'vite';
+import { resolve } from 'path';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+export default defineConfig({
+    build: {
+        rollupOptions: {
+            input: {
+                main: resolve(__dirname, 'index.html'),
+                not_found: resolve(__dirname, '404.html'),
+            },
+        },
+    },
+    publicDir: 'public',
+});
